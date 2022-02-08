873e010593fdd92a52a9f47ee66baad505201c60
Pricop Laurențiu <laurcons@outlook.com>
2022-02-08 19:15:10 +0200

    Added Remember Me functionality

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 class UserAuthenticator extends AbstractLoginFormAuthenticator
@@ -39,7 +40,8 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
             new UserBadge($username),
             new PasswordCredentials($password),
             [
-                new CsrfTokenBadge("auth", $csrf)
+                new CsrfTokenBadge("auth", $csrf),
+                new RememberMeBadge(),
             ]
         );
     }
