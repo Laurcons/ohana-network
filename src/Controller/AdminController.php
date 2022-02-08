92ae454fa50778b60b696155d2112b20d5c661d7
Pricop Laurențiu <laurcons@outlook.com>
2022-02-08 13:16:28 +0200

    Finished admin users page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\DTO\AdminResetPasswordDTO;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+/**
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
     public function index(): Response
     {
@@ -18,5 +30,83 @@ class AdminController extends AbstractController
                 ["Administration"]
             ]
         ]);
+    }
+
+    /**
+     * @Route("/users", name="admin_users", methods={"GET"})
+     */
+    public function users(
+        UserRepository $userRepo
+    ): Response
+    {
+        $allUsers = $userRepo->findAll();
+
+        return $this->render('admin/users.html.twig', [
+            'breadcrumbs' => [
+                ["Administration", "admin"],
+                ["Users"]
+            ],
+            'all_users' => $allUsers
+        ]);
+    }
+
+    /**
+     * @Route("/users/resetPassword", name="admin_users_resetPassword", methods={"POST"})
+     */
+    public function resetPassword(
+        Request $request,
+        UserPasswordHasherInterface $hasher,
+        UserRepository $userRepo,
+        ManagerRegistry $doctrine
+    ): Response {
+        (function () use ($request, $hasher, $userRepo, $doctrine) {
+
+            $uuid = $request->request->get('uuid');
+            $newPassword = $request->request->get('newPassword');
+
+            $user = $userRepo->findUuid($uuid);
+            if (!$user)
+                return;
+            if (array_search('ROLE_ADMIN', $user->getRoles()) === false)
+                return;
+
+            $hash = $hasher->hashPassword($user, $newPassword);
+
+            $user->setStatus(User::STATUS_PASSWORD_RESET);
+            $user->setPassword($hash);
+            $doctrine->getManager()->flush();
+        })();
+
+        return $this->redirectToRoute('admin_users');
+    }
+
+    /**
+     * @Route("/users/deactivate", name="admin_users_deactivate", methods={"POST"})
+     */
+    public function deactivate(
+        Request $request,
+        UserRepository $userRepo,
+        ManagerRegistry $doctrine
+    ): Response {
+        (function () use ($request, $userRepo, $doctrine) {
+
+            $uuid = $request->request->get('uuid');
+
+            $user = $userRepo->findUuid($uuid);
+            if (!$user)
+                return;
+            if (array_search('ROLE_ADMIN', $user->getRoles()) === false)
+                return;
+            if (array_search('ROLE_ACTIVATED', $user->getRoles()) === false)
+                return;
+
+            $user->setStatus(User::STATUS_NOT_ACTIVATED);
+            $roles = $user->getRoles();
+            $roles = array_filter($roles, function ($role) { return $role !== "ROLE_ACTIVATED"; });
+            $user->setRoles($roles);
+            $doctrine->getManager()->flush();
+        })();
+
+        return $this->redirectToRoute('admin_users');
     }
 }
