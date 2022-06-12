90d7c9f7ef9477190475e863ebd565e86fb62bfd
Pricop Laurențiu <laurcons@outlook.com>
2022-06-12 22:47:36 +0300

    Implemented new pronoun system

--- src/Controller/PreferencesController.php
+++ src/Controller/PreferencesController.php
@@ -17,6 +17,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class PreferencesController extends AbstractController
@@ -35,12 +36,23 @@ class PreferencesController extends AbstractController
         /** @var User */
         $currentUser = $this->getUser();
 
+        $pronounList = [];
+        foreach (User::getValidPronouns() as $val) {
+            $pronounList[$val] = $val;
+        }
+        if (!$currentUser->hasValidPronouns()) {
+            $currentUser->setPronouns("they/them/their/theirs");
+            $doctrine->getManager()->flush();
+        }
         $preferencesForm = $formFactory->createNamed('prefs', FormType::class, $currentUser)
             ->add('username', TextType::class, ['help' => 'Your username will only be used to login.'])
             ->add('birthdate', DateType::class, ['widget' => 'single_text'])
             ->add('realName', TextType::class, ['help' => 'Please make sure that you start with your family name, then your preferred given name, then any other given names (if you want). Do not use dashes. Eg. Pricop Laurentiu Constantin'])
             ->add('nickname', TextType::class, ['help' => 'Set a nickname that will be used across the Network.'])
-            ->add('pronouns', TextType::class, ['help' => 'Your pronouns will be used across the Network when referring to you. Please make sure they\'re in the <code>he/him/his</code> format, where the first pronoun is in nominative form ("he does"), the second is in dative form ("i give him"), and the third is in genitive form ("this is his stuff").', 'help_html' => true])
+            ->add('pronouns', ChoiceType::class, [
+                'help' => 'Your pronouns will be used across the Network when referring to you. Neo-pronouns are not supported.',
+                'choices' => $pronounList
+            ])
             ->add("submit", SubmitType::class, ['label' => "Update preferences"]);
         $preferencesForm->handleRequest($request);
 
--- src/Form/Type/ActivationFormType.php
+++ src/Form/Type/ActivationFormType.php
@@ -2,10 +2,10 @@
 
 namespace App\Form\Type;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -16,6 +16,10 @@ class ActivationFormType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $pronounList = [];
+        foreach (User::getValidPronouns() as $val) {
+            $pronounList[$val] = $val;
+        }
         $builder
             ->add('username', TextType::class, ['help' => 'Your username will only be used to login.'])
             // ->add('password', PasswordType::class, [])
@@ -34,7 +38,10 @@ class ActivationFormType extends AbstractType {
             ->add('birthdate', DateType::class, ['widget' => 'single_text'])
             ->add('realName', TextType::class, ['help' => 'Please make sure that you start with your family name, then your preferred given name, then any other given names (if you want). Do not use dashes. Eg. Pricop Laurentiu Constantin'])
             ->add('nickname', TextType::class, ['help' => 'Set a nickname that will be used across the Network.'])
-            ->add('pronouns', TextType::class, ['help' => 'Your pronouns will be used across the Network when referring to you. Please make sure they\'re in the <code>he/him/his</code> format, where the first pronoun is in nominative form ("he does"), the second is in dative form ("i give him"), and the third is in genitive form ("this is his stuff").', 'help_html' => true])
+            ->add('pronouns', ChoiceType::class, [
+                'help' => 'Your pronouns will be used across the Network when referring to you. Neo-pronouns are not supported.',
+                'choices' => $pronounList
+            ])
             ->add('submit', SubmitType::class, ['label' => "Finish registration"]);
     }
 }
